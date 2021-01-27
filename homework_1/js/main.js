d91d667d30566d36af941f77ccb7b1fad1172479
Rufus24-Loboda <gakuseinoyama@gmail.com>
2021-01-27 15:45:15 +0200

    Anime

---
+++ homework_1/js/main.js
@@ -0,0 +1,26 @@
+// Практичне завдання 1. Змінні і типи даних
+// 1
+// const str = "Привіт";
+// const num = 123;
+// const flag = true;
+// const txt = "true";
+// 2
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// const f = 6;
+// const g = 7;
+// const h = 8;
+// const i = 9;
+// const j = 10;
+// Практичне завдання 2. Взаємодія з користувачем. Основні оператори
+// 1
+const value = prompt('Please enter a name!');
+console.log(typeof(value));
+console.log(value);
+// 2
+const value = age ("Введіть скільки вам років");
+const value = ageFriend ("Введіть скільки років вашому другу");
+const pluse = age + ageFriend;
---
+++ homework_2/js/main.js
@@ -0,0 +1,23 @@
+// модуль 3. задачі
+// 1
+// const number = 24;
+// if (number === 1) {
+//     console.log("Сьогодні перше число");
+// } 
+// 2
+// const number = 23;
+// if (number %2 === 0) {
+//     console.log("Сьогодні перше число");
+// } else {
+//     console.log ("not Перше число")
+// }
+// 3
+const number = 5 % 2;
+if (number === 0) {
+    console.log( number + "grn" );
+} else {
+    console.log (number + "євро")
+}
+// 4
+const age = prompt('Скільки вам років?');
+if (age )
