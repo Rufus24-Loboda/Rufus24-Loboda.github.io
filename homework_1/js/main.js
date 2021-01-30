d8f932f97590f103ebe6d342e9379b2efa9fb34b
Rufus24-Loboda <gakuseinoyama@gmail.com>
2021-01-30 13:12:51 +0200

    naruuuuto_saaaske

--- homework_1/js/main.js
+++ homework_1/js/main.js
@@ -17,10 +17,49 @@
 // const j = 10;
 // Практичне завдання 2. Взаємодія з користувачем. Основні оператори
 // 1
-const value = prompt('Please enter a name!');
-console.log(typeof(value));
-console.log(value);
+// const value = prompt('Please enter a name!');
+// console.log(typeof(value));
+// console.log(value);
 // 2
-const value = age ("Введіть скільки вам років");
-const value = ageFriend ("Введіть скільки років вашому другу");
-const pluse = age + ageFriend;
+// let myage = 12;
+// let agefriend = 12;
+// const age = myage + agefriend;
+// alert(age);
+// 3
+// const a = 2;
+// const b = 3;
+// console.log(a + b);
+// 4
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// let a5 = "А про це нічого не сказано";
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+// 5
+// let a6 = 5 % 3;
+// let a7 = 3 % 5;
+// let a8 = 5 + '3';
+// let a9 = '5' - 3;
+// let a10 = 75 + 'кг';
+// alert(a6);
+// alert(a7);
+// alert(a8);
+// alert(a9);
+// alert(a10);
+// 6
+// let width = 10;
+// let height = 23;
+// let s = width * 2 + height * 2;
+// console.log(s);
+// 7
+// const heightC = 10;
+// const dC = 4;
+// const v = heightC * dC;
+// 8
+// let k = 3 ** 2 + 4 ** 2;
+// console.log(Math.sqrt(k));
