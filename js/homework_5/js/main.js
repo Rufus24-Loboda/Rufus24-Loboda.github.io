d09f02c28fff2bf6a9c7ab9c9ab8018758457cac
Rufus24-Loboda <gakuseinoyama@gmail.com>
2021-03-08 11:57:29 +0200

    inoob

---
+++ js/homework_5/js/main.js
@@ -0,0 +1,29 @@
+// Практичне завдання 5. Цикли
+// 1
+// let number = 0;
+// while (number < 10) {
+//   console.log(number);
+//   number += 1;
+// }
+// 2
+// for (let i = 0; i < 10; i += 1) {
+//   if (i%2===0){
+//       console.log("Число парне")
+//   } else { 
+//       console.log ("Число непарне")
+//     }
+//   console.log(i)
+// }
+// 3
+// let num = 20;
+// const maxNum = 29;
+// while(num < maxNum) {
+//     console.log(num);
+//     num ++ ;
+// }
+// 4
+// let  Name = '';
+// do {
+//     Name = prompt('вводите ім’я свого улюбленого героя', '');
+//   } while (Name.length <= 6);
+// console.log('треба щоб було менше 6 символів');
