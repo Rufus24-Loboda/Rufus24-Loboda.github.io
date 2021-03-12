f20cc5c1bbcca721c5aed2f2b10060573959f1ee
Rufus24-Loboda <gakuseinoyama@gmail.com>
2021-03-12 13:21:26 +0200

    hinataaaaaaaaaaaa

--- js/homework_6/js/main.js
+++ js/homework_6/js/main.js
@@ -6,3 +6,33 @@
 // console.log(Arr2[1]);
 // console.log(Arr2[1] + Arr1[1]);
 // 2
+// let Arr1 = [5, 7, 4, 6];
+// Arr1[4] = 22;
+// for (let i = 0; i < Arr1.length; i += 1) {
+//     console.log(Arr1[i]);
+//   }
+// 3
+// const Arr2 = [1, 'Ajax', false];
+// for (const client of Arr2 ) {
+//     for (let i = 0; i < Arr2.length; i += 1) {
+//         console.log(typeof Arr2[i]);
+//         console.log(Arr2[i]);
+//       }
+// }
+// 4
+const message = 'Welcome to Ukraine!';
+for (const character of message) {
+    console.log(character);
+  }
+  console.log(message.split('l'));
+  console.log(message.indexOf("l")); 
+  console.log(message.join(''));
+//   console.log(clients.join('l'));
+// 5
+// let styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-ролл');
+// styles[1] = 'Класика';
+// styles.shift('Джаз');
+// styles.unshift('Реп');
+// styles.unshift('Реггі');
+// console.log(styles);
